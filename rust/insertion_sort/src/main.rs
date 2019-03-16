c8e702301743d415f08e5a1ed89c88a9a8087eb2
ng3rdstmadgke <ng3rdstmagke@gmail.com>
2019-03-16 19:45:01 +0900

    テストを追加

--- rust/insertion_sort/src/main.rs
+++ rust/insertion_sort/src/main.rs
@@ -1,6 +1,12 @@
+use std::fmt::Write;
+
 fn main() {
     let n = read::<usize>();
-    let mut vec = read_as_vec::<u32>();
+    let vec = read_as_vec::<u16>();
+    insertion_sort(n, vec);
+}
+
+fn insertion_sort(n: usize, mut vec: Vec<u16>) -> Vec<u16> {
     for i in 0usize..n {
         let e = vec[i];
         for j in (0usize..i).rev() {
@@ -11,8 +17,20 @@ fn main() {
                 break;
             }
         }
-        println!("{}", vec.iter().map(|e| e.to_string()).collect::<Vec<String>>().join(" "));
+        println!("{}", join(' ', &vec));
     }
+    vec
+}
+
+fn join<T: std::fmt::Display>(delimiter: char, arr: &[T]) -> String {
+    let mut text = String::new();
+    for (i, e) in arr.iter().enumerate() {
+        if i > 0 {
+            text.push(delimiter);
+        }
+        write!(text, "{}", e).unwrap();
+    }
+    text
 }
 
 fn read<T: std::str::FromStr>() -> T {
@@ -28,3 +46,33 @@ fn read_as_vec<T: std::str::FromStr>() -> Vec<T> {
         .collect()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insertion_sort_1() {
+        let n = 6usize;
+        let vec: Vec<u16> = vec![5, 2, 4, 6, 1, 3];
+        assert_eq!(vec![1, 2, 3, 4, 5, 6],  insertion_sort(n, vec));
+    }
+
+    #[test]
+    fn test_insertion_sort_2() {
+        let n = 10usize;
+        let vec: Vec<u16> = vec![10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+        assert_eq!(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10], insertion_sort(n, vec));
+    }
+
+    #[test]
+    fn test_join_1() {
+        let vec: Vec<u16> = vec![1, 2, 3, 4, 5];
+        assert_eq!("1 2 3 4 5".to_string(), join(' ', &vec));
+    }
+
+    #[test]
+    fn test_join_2() {
+        let arr: [i32; 5] = [1, 2, 3, 4, 5];
+        assert_eq!("1 2 3 4 5".to_string(), join(' ', &arr));
+    }
+}
