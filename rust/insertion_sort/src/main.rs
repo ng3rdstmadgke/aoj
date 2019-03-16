32e0e6b09ac96a55c634f34fb8b7e8254871ea25
ng3rdstmadgke <ng3rdstmagke@gmail.com>
2019-03-16 17:49:26 +0900

    1_A_insertion_sort

--- rust/insertion_sort/src/main.rs
+++ rust/insertion_sort/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let n = read::<usize>();
+    let mut vec = read_as_vec::<u32>();
+    for i in 0usize..n {
+        let e = vec[i];
+        for j in (0usize..i).rev() {
+            if e < vec[j] {
+                vec[j + 1] = vec[j];
+                vec[j] = e;
+            } else {
+                break;
+            }
+        }
+        println!("{}", vec.iter().map(|e| e.to_string()).collect::<Vec<String>>().join(" "));
+    }
 }
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse::<T>().ok().unwrap()
+}
+
+fn read_as_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse::<T>().ok().unwrap())
+        .collect()
+}
+
